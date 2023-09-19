c51ad4ba6860d42a730baff018ab2b76668d7848
Eliezer Santos <99987950+EliezerS135@users.noreply.github.com>
2023-09-18 23:21:52 -0300

    CRUD-carros

---
+++ apiCarros/src/services/CarroService.js
@@ -0,0 +1,63 @@
+const DB = require('../db');
+
+module.exports = {
+    buscarTodos: () => {
+        return new Promise((aceito, rejeitado) => {
+            DB.query('SELECT * FROM carros', (error, results)=> {
+                if(error) { rejeitado(error); return; }
+                aceito(results);
+            });
+        });
+    }, 
+
+    buscarUm: (codigo) => {
+        return new Promise((aceito, rejeitado) => {
+            
+            DB.query('SELECT * FROM carros WHERE codigo = ?', [codigo], (error, results) => {
+                if(error) { rejeitado(error); return; }
+                if(results.length > 0){
+                    aceito(results[0]);
+                }else{
+                    aceito(false);
+                }
+            });
+        });
+    },
+
+    inserir: (modelo, placa) => {
+        return new Promise((aceito, rejeitado) => {
+            
+            DB.query('INSERT INTO carros (modelo, placa) VALUES(?,?)', 
+                [modelo, placa], 
+                (error, results) => {
+                    if(error) { rejeitado(error); return; }
+                    aceito(results.insertCodigo)
+                
+            });
+        });
+    },
+
+    alterar: (codigo, modelo, placa) => {
+        return new Promise((aceito, rejeitado) => {
+            
+            DB.query('UPDATE carros SET modelo = ?, placa = ? WHERE codigo = ?', 
+                [modelo, placa, codigo], 
+                (error, results) => {
+                    if(error) { rejeitado(error); return; }
+                    aceito(results)
+                
+            });
+        });
+    },
+
+    excluir: (codigo) => {
+        return new Promise((aceito, rejeitado) => {
+            DB.query('DELETE FROM carros WHERE codigo=?', [codigo], (error, results)=> {
+                if(error) { rejeitado(error); return; }
+                aceito(results);
+            });
+        });
+    }
+    
+
+};
